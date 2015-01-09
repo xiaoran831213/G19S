0c94b897ebf25f89a410fae5a96b6e24a9b7e858
xtong <tongxia1@msu.edu>
2015-01-09 11:14:49 -0500

    experiment with cached gene records, a memory conservertive analysis.

--- src/anl.R
+++ src/anl.R
@@ -62,7 +62,7 @@ ANL$bza <- function(gmx, pos, emx, rsp, cvr)
     out;
 }
     
-ANL$run<-function(gno, exp, rng, phe, rsp, cov, pcs=NULL, imp=T, FUN=ANL$one, ...)
+ANL$run1<-function(gno, exp, rng, phe, rsp, cov, pcs=NULL, imp=T, FUN=ANL$one, ...)
 {
     phe<-phe[, c("IID", rsp, cov)];
     phe<-HLP$clrMss(phe);
@@ -169,6 +169,113 @@ ANL$run<-function(gno, exp, rng, phe, rsp, cov, pcs=NULL, imp=T, FUN=ANL$one, ..
         MIN=pmin(out[,1L], out[,2L], out[,3L]),
         NVR=nvr,
         ERR=err, key='CHR,BP1,BP2');
+    out;
+}
+
+ANL$run2<-function(exp, rng, phe, rsp, cov, pcs=NULL, imp=T, FUN=ANL$one, ...)
+{
+    ## get individual list from the first gene record.
+    rng <- rng[, list(SEQ, CHR, BP1, BP2, GEN, PRB)];
+    idv <- HLP$gld(rng[1L,])$idv;
+
+    ## extract phenotype and covariants.
+    phe<-phe[, c("IID", rsp, cov)];
+    phe<-HLP$clrMss(phe);
+
+    ## align tables by indidivual id
+    iid <- sort(Reduce(intersect, list(idv, exp$idv, phe$IID)));
+    gdx <- match(iid, idv);
+    edx <- match(iid, exp$idv);
+    pdx <- match(iid, phe$IID);
+    
+    ## integrity check
+    stopifnot(identical(phe$IID[pdx], gno$idv[gdx]));
+    stopifnot(identical(phe$IID[pdx], exp$idv[edx]));
+    
+    ## response and covariate
+    rsp<-as.matrix(phe[pdx, rsp], rownames.force = F);   # response variable
+    cov<-as.matrix(phe[pdx, cov], rownames.force = F);   # covariants
+    if(!is.null(pcs))
+    {
+        cov <- cbind(cov, pcs[gdx,]);
+    }
+    
+    ## prepare output containers
+    nrg <- nrow(rng);
+    out <- matrix(data = NA, nrow = nrg, ncol = 3L); # 3 types of test
+    nvr <- rep.int(0L, nrg); # number of variants in gene ranges
+    err <- rep.int(NA, nrg); # error recorde
+    
+    ## iterate genome variants
+    require(data.table);
+    for(i in 1L:nrg)
+    {
+        ## get expression matrix
+        prb<-rng[i,PRB];
+        emx<-exp$emx[exp$prb==prb, edx, drop=F]; # row->probe, col->indvidual
+        if(length(emx)==0L)
+        {
+            err[i] <- 'EXP=N'; ## no expression data
+            next;
+        }
+        
+        ## get testing range i and pick variants within
+        gen <- HLP$gld(rng[i,]);
+        gmx <- gen$gmx;
+        pos <- gen$map$POS;
+        
+        ## record number of variants in the i.th gene range.
+        if(is.null(gmx))
+        {
+            err[i]<-'NVR=0' # no variants in gene i.
+            next;
+        }
+        nvr[i] <- nrow(gmx);
+
+        ## save population mean genotype value for later imputation
+        avg<-apply(gmx, 1L, mean, na.rm=T); 
+        
+        ## exclude unavailable individual
+        gmx<-gmx[,gdx, drop=F];  # row->variant, col->individual
+        
+        ## exclude degenerated variants
+        idx<-HLP$gmxClr(gmx);
+        if(length(idx)==0L)
+        {
+            err[i]<-'NES=0' ## number of effective sample is zero
+            next;
+        }
+        gmx<-gmx[idx,,drop=F];
+        
+        ## asign population mean genotype value to NA if imputation
+        ## is requested.
+        if(imp)
+        {
+            for(j in 1L:nrow(gmx))
+                gmx[j, is.na(gmx[j,])]<-avg[j];
+        }
+        
+        ## call U sta
+        gmx<-t(gmx); # row->individual, col->genomic variant
+        emx<-t(emx); # row->individual, col->RNA probe
+        r<-FUN(gmx, emx, rsp, cov, ...);
+        if (inherits(r, "try-error"))
+        {
+            err[i] <- errMsg(p);
+            next;
+        }
+        
+        ## record result.
+        out[i,] <- c(r$GT.p, r$G.p, r$T.p);
+
+        ## report progress.
+        HLP$shwPrg(nrg, i, 256L);
+    }
+    out<-data.table(
+        rng,
+        GT=out[,1L], G=out[,2L], T=out[,3L],
+        MIN=pmin(out[,1L], out[,2L], out[,3L]),
+        NVR=nvr, ERR=err, key='CHR,BP1,BP2');
     out;
 }
 
